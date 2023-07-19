ca759edaf37994116b6ef219720132ec959dc9e3
EvanKelley <sionnach1993@gmail.com>
2023-07-19 11:23:35 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+// Middleware to parse incoming requests as JSON
+app.use(express.json());
+
+// Include your routes
+app.use('/api', routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
