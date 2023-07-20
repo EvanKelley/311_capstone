3430744dbebdb57ff3097f3bc989c4220968f0b9
EvanKelley <sionnach1993@gmail.com>
2023-07-20 11:00:06 -0500

    third commit

---
+++ src/controller.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const API_BASE_URL = 'https://www.dnd5eapi.co/api/';
+
+// Function to fetch rules by name
+async function searchRulesByName(ruleName) {
+  try {
+    const response = await axios.get(`${API_BASE_URL}rules/?name=${encodeURIComponent(ruleName)}`);
+    return response.data;
+  } catch (error) {
+    // Handle error, such as invalid rule name or API connection issue
+    console.error('Error fetching rules:', error.message);
+    throw error;
+  }
+};
+
+// Function to create a new character
+async function createCharacter(req, res) {
+  const { name, race, class, ...otherDetails } = req.body;
+
+  // Validate the incoming data, if needed
+
+  try {
+    // Perform database insertion to create the new character
+    const result = await db.query('INSERT INTO characters (name, race, class, ...) VALUES (?, ?, ?, ...)', [name, race, class, ...]);
+
+    // If the character was successfully created, return a success response
+    return res.status(201).json({ success: true, message: 'Character created successfully', characterId: result.insertId });
+  } catch (error) {
+    // Handle any errors that occurred during character creation
+    console.error('Error creating character:', error);
+    return res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+};
+  
+  module.exports = {
+    searchRulesByName,
+    createCharacter,
+  };
+
+// Usage example
+searchRulesByName('spellcasting'); // Replace 'spellcasting' with your desired rule name
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,42 @@ const router = express.Router();
 const db = require('./db');
 
 // Define routes here
+// User Authentication
+// ===================
+// POST /api/signup: Create a new user account.
+// POST /api/login: Log in an existing user and obtain an authentication token (JWT).
+// GET /api/logout: Log out the currently logged-in user.
+
+
+// Character Management
+// ====================
+// POST /api/characters: Create a new character for the authenticated user.
+router.post("/api/characters", controller.createCharacter);
+// GET /api/characters: Retrieve a list of characters belonging to the authenticated user.
+router.get("/api/characters", controller.getCharacters);
+// GET /api/characters/:characterId: Retrieve details of a specific character.
+// PUT /api/characters/:characterId: Update details of a specific character.
+// DELETE /api/characters/:characterId: Delete a specific character.
+
+
+// Abilities and Items
+// ===================
+// POST /api/characters/:characterId/abilities: Add a new ability to a character.
+// GET /api/characters/:characterId/abilities: Retrieve a list of abilities for a character.
+// PUT /api/characters/:characterId/abilities/:abilityId: Update details of a specific ability.
+// DELETE /api/characters/:characterId/abilities/:abilityId: Delete a specific ability from a character.
+// POST /api/characters/:characterId/items: Add a new item to a character's inventory.
+// GET /api/characters/:characterId/items: Retrieve a list of items in a character's inventory.
+// PUT /api/characters/:characterId/items/:itemId: Update details of a specific item.
+// DELETE /api/characters/:characterId/items/:itemId: Delete a specific item from a character's inventory.
+
+
+// Spells
+// ======
+// GET /api/spells: Retrieve a list of all available spells.
+// GET /api/spells/:spellId: Retrieve details of a specific spell.
+// Rule Lookup:
+// GET /api/rules: Retrieve a list of available rule references.
+// GET /api/rules/:ruleId: Retrieve details of a specific rule reference.
 
 module.exports = router;
