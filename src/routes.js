e9dcb66db5dda182de48a1f6291d368636573c8f
EvanKelley <sionnach1993@gmail.com>
2023-07-20 12:02:45 -0500

    fourth commit

--- src/routes.js
+++ src/routes.js
@@ -1,44 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('./controller');
 const db = require('./db');
 
-// Define routes here
-// User Authentication
-// ===================
-// POST /api/signup: Create a new user account.
-// POST /api/login: Log in an existing user and obtain an authentication token (JWT).
-// GET /api/logout: Log out the currently logged-in user.
+
+// User Authentication Routes
+// ==========================
+// Create a new user account.
+router.post("/api/signup", controller.registerUser);
+// Log in an existing user and obtain an authentication token (JWT).
+router.post("/api/login", controller.loginUser);
+// Log out the currently logged-in user.
+router.get("api/logout", controller.logoutUser);
 
 
-// Character Management
-// ====================
-// POST /api/characters: Create a new character for the authenticated user.
+// Character Management Routes
+// ===========================
+// Create a new character for the authenticated user.
 router.post("/api/characters", controller.createCharacter);
-// GET /api/characters: Retrieve a list of characters belonging to the authenticated user.
+// Retrieve a list of characters belonging to the authenticated user.
 router.get("/api/characters", controller.getCharacters);
-// GET /api/characters/:characterId: Retrieve details of a specific character.
-// PUT /api/characters/:characterId: Update details of a specific character.
-// DELETE /api/characters/:characterId: Delete a specific character.
+// Retrieve details of a specific character.
+router.get("/api/characters/:characterId", controller.getCharacterById);
+// Update details of a specific character.
+router.put("/api/characters/:characterId", controller.updateCharacter);
+// Delete a specific character.
+router.delete("/api/characters/:characterId", controller.deleteCharacter);
 
 
-// Abilities and Items
-// ===================
-// POST /api/characters/:characterId/abilities: Add a new ability to a character.
-// GET /api/characters/:characterId/abilities: Retrieve a list of abilities for a character.
-// PUT /api/characters/:characterId/abilities/:abilityId: Update details of a specific ability.
-// DELETE /api/characters/:characterId/abilities/:abilityId: Delete a specific ability from a character.
-// POST /api/characters/:characterId/items: Add a new item to a character's inventory.
-// GET /api/characters/:characterId/items: Retrieve a list of items in a character's inventory.
-// PUT /api/characters/:characterId/items/:itemId: Update details of a specific item.
-// DELETE /api/characters/:characterId/items/:itemId: Delete a specific item from a character's inventory.
+// Abilities and Items Routes
+// ==========================
+// Add a new ability to a character.
+router.post("/api/characters/:characterId/abilities", controller.addAbility);
+// Retrieve a list of abilities for a character.
+router.get("/api/characters/:characterId/abilities", controller.getAbilities);
+// Update details of a specific ability.
+router.put("/api/characters/:characterId/abilities/:abilityId", controller.updateAbility);
+// Delete a specific ability from a character.
+router.delete("/api/characters/:characterId/abilities/:abilityId", controller.deleteAbility);
+// Add a new item to a character's inventory.
+router.post("/api/characters/:characterId/items", controller.addItem);
+// Retrieve a list of items in a character's inventory.
+router.get("/api/characters/:characterId/items", controller.getItems);
+// Update details of a specific item.
+router.put("/api/characters/:characterId/items/:itemId", controller.updateItem);
+// Delete a specific item from a character's inventory.
+router.delete("/api/characters/:characterId/items/:itemId", controller.deleteItem);
+
+// Spell Routes
+// ============
+// Retrieve a list of all available spells.
+router.get("/api/spells", controller.getSpells);
+// Retrieve details of a specific spell.
+router.get("/api/spells/:spellId", controller.getSpellById)
+
+// Rules Routes
+// ============
+// Retrieve a list of available rule references.
+router.get("/api/rules", controller.getRules)
+// Retrieve details of a specific rule reference.
+router.get("/api/rules/:ruleId", controller.getRuleById)
 
 
-// Spells
-// ======
-// GET /api/spells: Retrieve a list of all available spells.
-// GET /api/spells/:spellId: Retrieve details of a specific spell.
-// Rule Lookup:
-// GET /api/rules: Retrieve a list of available rule references.
-// GET /api/rules/:ruleId: Retrieve details of a specific rule reference.
-
+// Export
 module.exports = router;
