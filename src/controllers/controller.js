1761addc1e6f1c51bd6e3601edcdf9730a373b62
EvanKelley <sionnach1993@gmail.com>
2023-07-23 00:20:49 -0500

    eighth commit

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,3 +1,4 @@
+
 const axios = require('axios');
 const API_BASE_URL = 'https://www.dnd5eapi.co/api/';
 const db = require('../db');
@@ -5,7 +6,6 @@ const db = require('../db');
 
 
 // Create a new character
-// ======================
 async function createCharacter(req, res) {
   const { name, race, characterClass, ...otherAttributes } = req.body;
 
@@ -41,24 +41,96 @@ async function createCharacter(req, res) {
 };
 
 
-// List users characters
-// ===================== 
+// Retrieve a list of characters belonging to a user
+async function getCharacters(req, res) {
+  // Assuming implementation of user authentication and have obtained the user's ID
+  const userId = req.user.id;
+
+  try {
+    // Fetch characters from the database for the authenticated user
+    const characters = await db.query('SELECT * FROM characters WHERE user_id = ?', [userId]);
+
+    // Send the list of characters as the response
+    res.status(200).json(characters);
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error fetching characters:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+} 
 
 
 // Retreive specific character
-// ===========================
+async function getCharacterById(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+
+  try {
+    // Fetch the character from the database based on the characterId
+    const character = await db.query('SELECT * FROM characters WHERE character_id = ?', [characterId]);
+
+    // Check if the character exists
+    if (!character || character.length === 0) {
+      return res.status(404).json({ error: 'Character not found.' });
+    }
+
+    // Send the character data as the response
+    res.status(200).json(character[0]);
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error fetching character:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
 // Update specific character
-// =========================
+async function updateCharacter(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+  const updatedCharacterData = req.body; // Get the updated character data from the request body
+
+  try {
+    // Update the character in the database based on the characterId
+    const result = await db.query('UPDATE characters SET ? WHERE character_id = ?', [updatedCharacterData, characterId]);
+
+    // Check if the character was updated successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Character not found.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Character updated successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error updating character:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
 
 
 // Delete specific character
-// =========================
+async function deleteCharacter(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+
+  try {
+    // Delete the character from the database based on the characterId
+    const result = await db.query('DELETE FROM characters WHERE character_id = ?', [characterId]);
+
+    // Check if the character was deleted successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Character not found.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Character deleted successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error deleting character:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
 // Add a new ability to a character
-// ================================
 async function addAbility(req, res) {
   const { characterId } = req.params;
   const { abilityName, abilityDescription, ...otherAttributes } = req.body;
@@ -83,35 +155,161 @@ async function addAbility(req, res) {
 
 
 // Retrieve a list of abilities for a character
-// ============================================
+async function getAbilities(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
 
+  try {
+    // Fetch the abilities from the database for the specific character
+    const abilities = await db.query('SELECT * FROM abilities WHERE character_id = ?', [characterId]);
+
+    // Send the list of abilities as the response
+    res.status(200).json(abilities);
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error fetching abilities:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 // Update details of a specific ability
-// ====================================
+async function updateAbility(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+  const abilityId = req.params.abilityId; // Extract the abilityId from request parameters
+  const updatedAbilityData = req.body; // Get the updated ability data from the request body
+
+  try {
+    // Update the ability in the database based on the characterId and abilityId
+    const result = await db.query('UPDATE abilities SET ? WHERE character_id = ? AND ability_id = ?', [updatedAbilityData, characterId, abilityId]);
+
+    // Check if the ability was updated successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Ability not found.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Ability updated successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error updating ability:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
 // Delete a specific ability from a character
-// ==========================================
+async function deleteAbility(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+  const abilityId = req.params.abilityId; // Extract the abilityId from request parameters
+
+  try {
+    // Delete the ability from the database based on the characterId and abilityId
+    const result = await db.query('DELETE FROM abilities WHERE character_id = ? AND ability_id = ?', [characterId, abilityId]);
+
+    // Check if the ability was deleted successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Ability not found for this character.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Ability deleted successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error deleting ability:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
 // Add a new item to a character's inventory
-// =========================================
+async function addItem(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+  const newItemData = req.body; // Get the new item data from the request body
 
+  try {
+    // Add the new item to the database with the specified characterId
+    const result = await db.query('INSERT INTO items SET character_id = ?, ?', [characterId, newItemData]);
+
+    // Check if the item was added successfully
+    if (result.affectedRows === 0) {
+      return res.status(500).json({ error: 'Error adding item to inventory.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Item added to inventory successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error adding item:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}; 
 
 // Retrieve a list of items in a character's inventory
-// ===================================================
+async function getItems(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
 
+  try {
+    // Fetch the items from the database for the specific character
+    const items = await db.query('SELECT * FROM items WHERE character_id = ?', [characterId]);
+
+    // Send the list of items as the response
+    res.status(200).json(items);
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error fetching items:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 // Update details of a specific item
-// =================================
+async function updateItem(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+  const itemId = req.params.itemId; // Extract the itemId from request parameters
+  const updatedItemData = req.body; // Get the updated item data from the request body
+
+  try {
+    // Update the item in the database based on the characterId and itemId
+    const result = await db.query('UPDATE items SET ? WHERE character_id = ? AND item_id = ?', [updatedItemData, characterId, itemId]);
+
+    // Check if the item was updated successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Item not found for this character.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Item updated successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error updating item:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
 // Delete a specific item from a character's inventory
-// ===================================================
+async function deleteItem(req, res) {
+  const characterId = req.params.characterId; // Extract the characterId from request parameters
+  const itemId = req.params.itemId; // Extract the itemId from request parameters
+
+  try {
+    // Delete the item from the database based on the characterId and itemId
+    const result = await db.query('DELETE FROM items WHERE character_id = ? AND item_id = ?', [characterId, itemId]);
+
+    // Check if the item was deleted successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Item not found for this character.' });
+    }
+
+    // Send a success message as the response
+    res.status(200).json({ message: 'Item deleted successfully.' });
+  } catch (error) {
+    // Handle error, such as database query issue
+    console.error('Error deleting item:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
 // Retrieve a list of all available spells
-// =======================================
 async function getSpells(req, res) {
   try {
     // Make a GET request to the API endpoint for spells
@@ -125,20 +323,53 @@ async function getSpells(req, res) {
     console.error('Error fetching spells:', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
 
 
 
 // Retrieve details of a specific spell
-// ====================================
+async function getSpellByName(spellName) {
+  try {
+    // Make a get request to the API for spells with specified anme
+    const response = await axios.get(`${API_BASE_URL}spells/?name=${encodeURIComponent(spellName)}`);
+    const spellData = response.data;
 
+    // If the ruleData is an empty array, it means the rule with the given name does not exist
+    if (Array.isArray(spellData) && spellData.length === 0) {
+      throw new Error(`Spell "${spellName}" not found.`);
+    }   
+    
+    // If the spellData is an array, assume the first item is the spell data
+    if (Array.isArray(spellData)) {
+      return spellData[0];
+    }
+
+    // If the spellData is not an array, return it as is
+    return spellData;
+  } catch (error) {
+    // Handle error, such as invalid spell name or API connection issue
+    console.error('Error fetching spells', error.message);
+    throw error;
+  }
+};
 
 // Retrieve a list of available rule references
-// ============================================
+async function getRules(req, res) {
+  try {
+    // Make a GET request to the API endpoint for rules
+    const response = await axios.get(`${API_BASE_URL}rules`);
+    const rulesList = response.data;
 
+    // Send the list of spells as the response
+    res.status(200).json(rulesList);
+  } catch (error) {
+    // Handle error, such as API connection issue
+    console.error('Error fetching rules:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 // Look up a specific rule reference
-// =================================
 async function getRuleByName(ruleName) {
   try {
     const response = await axios.get(`${API_BASE_URL}rules/?name=${encodeURIComponent(ruleName)}`);
@@ -165,10 +396,21 @@ async function getRuleByName(ruleName) {
 
 
 // Exports
-// =======
   module.exports = {
     createCharacter,
+    getCharacterById,
+    updateCharacter,
+    deleteCharacter,
     addAbility,
+    getAbilities,
+    updateAbility,
+    deleteAbility,
+    addItem,
+    getItems,
+    updateItem,
+    deleteItem,
     getSpells,
+    getSpellByName,
+    getRules,
     getRuleByName
   };
