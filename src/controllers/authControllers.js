1df567505ab87ee5781fc7f2027c5ccdac06eba6
EvanKelley <sionnach1993@gmail.com>
2023-11-02 14:08:04 -0500

    push10

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -113,56 +113,6 @@ let loginUser = function (req, res) {
   });
 };
 
-// let loginUser = function (req, res) {
-
-//   let email = req.body.email;
-//   let password = req.body.password;
-
-//   let sql = "SELECT user_id, password FROM users WHERE email = ?";
-//   let params = [email];
-
-//   db.query(sql, params, async function (err, results) {
-//       if (err) {
-//           console.error("Database error while retrieving user data:", err);
-//           return res.sendStatus(500); // Internal Server Error
-//       }
-
-//       if (results.length === 0) {
-//           console.log("No user found for email:", email);
-//           return res.sendStatus(401); // Unauthorized
-//       }
-
-//       if (results.length > 1) {
-//           console.error("Returned more than 1 user for the email:", email);
-//           return res.sendStatus(500); // Internal Server Error
-//       }
-
-//       // At this point, there's only one user with the provided email
-//       let storedHash = results[0].password;
-//       let storedId = results[0].user_id;
-
-//       try {
-//           let pass = await argon.verify(storedHash, password);
-//           if (pass) {
-//               // Generate a token and send it back
-//               let token = {
-//                   user_id: storedId,
-//                   email: email
-//               };
-
-//               // Token is good for 1 day
-//               let signedToken = jwt.sign(token, process.env.JWT_SECRET, { expiresIn: 86400 });
-//               res.json(signedToken);
-//           } else {
-//               console.log("Password verification failed for email:", email);
-//               res.sendStatus(401); // Unauthorized
-//           }
-//       } catch (err) {
-//           console.error("Failed when verifying the hash:", err);
-//           res.sendStatus(500); // Internal Server Error
-//       }
-//   });
-// };
 
 
   module.exports = {
